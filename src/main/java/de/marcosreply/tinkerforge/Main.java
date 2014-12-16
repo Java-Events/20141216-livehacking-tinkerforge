dc66fd382eecdc6299afed4dab006e206f028ff0
Sven Ruppert <sven.ruppert@gmail.com>
2014-12-16 17:14:34 +0100

    erste Main

---
+++ src/main/java/de/marcosreply/tinkerforge/Main.java
@@ -0,0 +1,12 @@
+package de.marcosreply.tinkerforge;
+
+/**
+ * Created by Sven Ruppert on 16.12.2014.
+ */
+public class Main {
+
+
+  public static void main(String[] args) {
+    System.out.println("args = " + args);
+  }
+}
